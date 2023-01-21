7a093ffb018667bf0933b4a0cf9ddb1bfa7939ce
Arturs Aboltins <arthuraboltins@gmail.com>
2023-01-21 13:20:07 +0000

    create pseudocode and initial functions

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// Create a simple blackjack game with the following properties:
+var playerHand = 0;
+var dealerHand = 0;
+
+// Deals the player a random number between 4 and 21 inclusive.
+function dealToPlayer(){
+// If the player has 21 they win! 
+
+}
+
+   
+// Deals a random number between 2 and 11 inclusive to the dealer.
+function dealToDealer(){
+
+}
+
+
+
+function playerHit(){
+// If the player hits, add a number between 2 and 11.
+// If the player goes over 21 the lose. 
+}
+   
+
+     
+// If the player has 21, stay for them.
+     
+// Repeat until they choose to stay or they bust
+     
+// Once player stays add a number between 2 and 11 to the dealer's hand.
+     
+// If dealer number is less than 17 add another number
+     
+// Repeat until the number is over over 17 but less than 21, or if the dealer goes over 21
+     
+// If dealer goes over 21 they lose.
+  
+// Once dealer stops, and neither player bust, compare each number to 21. Whoever is closer wins!
+
+// Pseudocode the outline of your game before you write any code.
+
+// Create functions if you begin to repeat yourself.
